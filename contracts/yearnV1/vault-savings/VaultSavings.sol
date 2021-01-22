b3bf94118ee3dd57412aca3477463e28444876b1
Pavel Rubin <pr@akropolis.io>
2021-01-22 11:21:16 +0300

    DD-1695 Add withdraw and some other improvements

--- contracts/yearnV1/vault-savings/VaultSavings.sol
+++ contracts/yearnV1/vault-savings/VaultSavings.sol
@@ -105,13 +105,6 @@ contract VaultSavings is IVaultSavings, OwnableUpgradeable, ReentrancyGuardUpgra
 
     }
 
-    function _getCurveFiTokenPosition(CurveFiInfo storage cf, address token) internal returns(uint256) {
-        for(uint256 i=0; i<cf.tokens.length; i++){
-            if(cf.tokens[i] == token) return i;
-        }
-        revert("Token not found in CurveFi");
-    }
-
     function _curveFiDeposit(CurveFiInfo storage cf, address cfLPToken, uint256[] memory _amounts, uint256 _minCurveLPAmount) internal 
     returns(uint256 curveLPAmount){
         // Transfer underlying tokens
@@ -169,6 +162,93 @@ contract VaultSavings is IVaultSavings, OwnableUpgradeable, ReentrancyGuardUpgra
         emit  Deposit(_vault, msg.sender, _amount, lpAmount);
     }
 
+    function withdraw(address[] calldata _vaults, address[][] calldata _tokens, uint256[][] calldata _amounts, uint256[] calldata maxConvertAmounts) external nonReentrant whenNotPaused {
+        require(_vaults.length > 0, "Nothing to withdraw");
+        require(_vaults.length == _tokens.length, "Size of tokens array does not match vaults");
+        require(_vaults.length == _amounts.length, "Size of amounts array does not match vaults");
+
+        for(uint256 i=0; i<_vaults.length; i++){
+            require(_tokens[i].length == _amounts[i].length, "Size of tokens array does not match amounts");
+            //_withdraw_one_vault(_vaults[i], _tokens[i], _amounts[i], minConvertAmounts[i]);
+        }
+    
+    }
+
+    function _withdraw_one_vault(address _vault, address[] memory _tokens, uint256[] memory _amounts, uint256 maxConvertAmount) internal 
+    returns(uint256){
+        CurveFiInfo storage cf = vaults[_vault].curveFi;
+
+        address baseToken = IVault(_vault).token();
+        uint256 baseAmount;
+
+        uint256[] memory amnts = new uint256[](cf.tokens.length);
+        bool cfWithdrawRequired;
+        for(uint256 t=0; t<_tokens.length; t++){
+            if(_tokens[t] == baseToken){
+                baseAmount = _amounts[t];
+            }else{
+                uint256 pos = _getCurveFiTokenPosition(cf, _tokens[t]);
+                amnts[pos] = _amounts[t];
+                if(amnts[pos] > 0) cfWithdrawRequired = true;
+            }
+        }
+        
+        uint256 fullWithdrawAmount = baseAmount;
+        uint256 cfWithdrawAmount;
+        if(cfWithdrawRequired) {
+            cfWithdrawAmount = (maxConvertAmount != 0)? maxConvertAmount : _curveFiCalculateWithdrawAmount(cf, amnts);
+            fullWithdrawAmount = fullWithdrawAmount.add(cfWithdrawAmount);
+        }
+        
+
+        uint256 withdrawShares = IVault(_vault).getPricePerFullShare().mul(fullWithdrawAmount);
+        IERC20Upgradeable(_vault).safeTransferFrom(msg.sender, address(this), withdrawShares);
+        IVault(_vault).withdraw(withdrawShares);
+        emit Withdraw(_vault, msg.sender, fullWithdrawAmount, withdrawShares);
+
+        if(baseAmount > 0) {
+            IERC20Upgradeable(baseToken).safeTransfer(msg.sender, baseAmount);
+        }
+        if(cfWithdrawRequired){
+            uint256 curveLPAmount = _curveFiWithdraw(cf, baseToken, amnts, cfWithdrawAmount);
+        }
+
+        // uint256 curveLPLeft = IERC20Upgradeable(baseToken).balanceOf(address(this));
+        // TODO: somehow handle this tokens
+
+    }
+
+    function _curveFiCalculateWithdrawAmount(CurveFiInfo storage cf, uint256[] memory amounts) internal returns(uint256 withdrawAmount) {
+        ICurveFiDeposit cfd = ICurveFiDeposit(cf.deposit);
+        for(uint256 i=0; i<amounts.length; i++){
+            withdrawAmount = withdrawAmount.add(cfd.calc_withdraw_one_coin(amounts[i], int128(i)));
+        }
+    }
+
+
+    function _curveFiWithdraw(CurveFiInfo storage cf, address cfLPToken, uint256[] memory _amounts, uint256 _maxCurveLPAmount) internal 
+    returns(uint256 curveLPAmount){
+
+        //NOTE: cf.deposit is trusted contract and we have nonReentrant modifier on top level
+        uint256 balanceBefore = IERC20Upgradeable(cfLPToken).balanceOf(address(this));
+        if(_amounts.length == 4){
+            ICurveFiDeposit4(cf.deposit).remove_liquidity_imbalance(ArrayConversions.convertUint256Array4(_amounts), _maxCurveLPAmount);
+        }else if(_amounts.length == 3) {
+            ICurveFiDeposit3(cf.deposit).remove_liquidity_imbalance(ArrayConversions.convertUint256Array3(_amounts), _maxCurveLPAmount);
+        }
+        uint256 balanceAfter = IERC20Upgradeable(cfLPToken).balanceOf(address(this));
+        curveLPAmount = balanceAfter.sub(balanceBefore);
+
+
+        // Transfer underlying tokens
+        for(uint256 i=0; i<cf.tokens.length; i++){
+            if(_amounts[i] > 0){
+                IERC20Upgradeable(cf.tokens[i]).safeTransfer(msg.sender, _amounts[i]);
+            }
+        }
+        emit CurveWithdraw(cf.deposit, msg.sender, _amounts, curveLPAmount);
+    }
+
 
     function withdraw(address[] calldata _vaults, uint256[] calldata _amounts) external override nonReentrant whenNotPaused {
         require(_vaults.length == _amounts.length, "Size of arrays does not match");
@@ -202,17 +282,22 @@ contract VaultSavings is IVaultSavings, OwnableUpgradeable, ReentrancyGuardUpgra
         emit Withdraw(_vault, msg.sender, baseAmount, _amount);
     }
 
-    function registerVault(address _vault) external override onlyOwner {
+    function registerVault(address _vault, address _cfDeposit, uint256 tokensCount) external override onlyOwner {
         require(!isVaultRegistered(_vault), "Vault is already registered");
 
         registeredVaults.push(_vault);
+
+        address[] memory tkns = new address[](tokensCount);
+        for(uint256 i=0; i<tokensCount; i++){
+            tkns[i] = ICurveFiDeposit(_cfDeposit).underlying_coins(int128(i));
+        }
 
         vaults[_vault] = VaultInfo({
             isActive: true,
             blockNumber: block.number,
             curveFi: CurveFiInfo({
-                deposit: address(0),
-                tokens: new address[](0)
+                deposit: _cfDeposit,
+                tokens: tkns
             })
         });
 
@@ -290,4 +375,12 @@ contract VaultSavings is IVaultSavings, OwnableUpgradeable, ReentrancyGuardUpgra
             }
         }
     }   
+
+    function _getCurveFiTokenPosition(CurveFiInfo storage cf, address token) internal returns(uint256) {
+        for(uint256 i=0; i<cf.tokens.length; i++){
+            if(cf.tokens[i] == token) return i;
+        }
+        revert("Token not found in CurveFi");
+    }
+
 }
