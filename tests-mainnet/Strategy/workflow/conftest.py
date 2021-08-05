f9088d9e3304f73b2771f4d62fcc96dcd0469927
joshualyguessennd <joshualyguessennd@icloud.com>
2021-08-05 15:37:49 +0400

    fix: ignore

--- tests-mainnet/Strategy/workflow/conftest.py
+++ tests-mainnet/Strategy/workflow/conftest.py
@@ -34,25 +34,6 @@ def vault_savings(env_settings, Contract):
     yield Contract.from_explorer(os.getenv("VAULTSAVINGV2_PROXY"), as_proxy_for=os.getenv("VAULTSAVINGV2"))
 
 
-# @pytest.fixture(scope="module")
-# def vault_tricrypto(env_settings, Contract):
-#     yield Contract.from_explorer(os.getenv("TRYCRYPTO"), as_proxy_for=os.getenv("TRYCRYPTO_PROXY"))
-
-
-# @pytest.fixture(scope="module")
-# def governance(env_settings):
-#     yield accounts.at(os.getenv("GOVERNANCE"), force=True)
-
-
-# @pytest.fixture(scope="module")
-# def lpToken(env_settings, Contract):
-#     yield Contract.from_explorer(os.getenv("LPTOKEN"), as_proxy_for=None)
-
-
-# @pytest.fixture(scope="module")
-# def lptoken_owner(env_settings):
-#     yield accounts.at(os.getenv("LPTOKEN_OWNER"), force=True)
-
 
 @pytest.fixture(scope="module")
 def rewards(env_settings, Contract):
