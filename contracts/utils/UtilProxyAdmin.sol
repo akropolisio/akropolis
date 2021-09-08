3234f1c2b975102822c609347a25fa7e3885ad08
joshualyguessennd <joshualyguessennd@icloud.com>
2021-09-09 01:07:48 +0400

    fix:clean

--- contracts/utils/UtilProxyAdmin.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity ^0.6.12;
-pragma experimental ABIEncoderV2;
-
-import "@openzeppelin/contracts/proxy/ProxyAdmin.sol";
-
-contract UtilProxyAdmin is ProxyAdmin {}
